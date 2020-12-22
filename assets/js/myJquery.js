fbc04787e514069baee503637061b607df68d2f5
shartouny <shartouny@outlook.com>
2020-12-22 17:08:52 +0200

    m

--- assets/js/myJquery.js
+++ assets/js/myJquery.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-    $('#items-content').owlCarousel({
+    $('#img-gallery .owl-carousel').owlCarousel({
         loop: true,
         margin: 30,
         nav: false,
+        dots: false,
         responsive: {
             0: {
                 items: 1
@@ -15,54 +16,4 @@ $(document).ready(function() {
             }
         }
     });
-    $('#products-content').owlCarousel({
-        loop: false,
-        margin: 30,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    });
-    $('#news-content').owlCarousel({
-        loop: false,
-        margin: 30,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            }
-        }
-    });
-
-    // $('.brand:nth-child(odd)').attr('data-aos', 'fade-left');
-    // $('.brand:nth-child(even)').attr('data-aos', 'fade-right');
-    // AOS.init({
-    //     offset: 100,
-    //     duration: 1000
-    // });
-    $('.scroll-to').click(function() {
-        var getElement = $(this).attr('href');
-        if ($(getElement).length) {
-            var getOffset = $(getElement).offset().top;
-            $('html,body').animate({
-                scrollTop: getOffset - 50
-            }, 500);
-        }
-        return false;
-    });
-
 });
